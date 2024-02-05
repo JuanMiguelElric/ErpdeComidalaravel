3a72391dd1fe0dc8cf1b67ac982ff5104378cd1c
JuanMiguelElric <oliveirajuan243@gmail.com>
2024-02-05 17:05:08 -0300

    desenvolvimento da pagina de cadastro ao produto

--- resources/views/Cardapio/cadastro.blade.php
+++ resources/views/Cardapio/cadastro.blade.php
@@ -7,6 +7,15 @@
 <div>
     <form action="">
     {{ csrf_field() }}
+    <label for="">Nome</label>
+    <input type="text">
+    <label for="">Ingrediente</label> <br>
+    <textarea name="" id="" cols="30" rows="10"></textarea> <br>
+    <label for="">preço</label>
+    <input type="text" name="" id="">
+    <label for="">Codigo</label>
+    <input type="text"> <br>
+    <button class="btn btn-sucess btn-lg btn-block">Cadastrar</button>
     </form>
 </div>
 
