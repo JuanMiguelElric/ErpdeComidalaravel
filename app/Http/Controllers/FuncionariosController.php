798a5bd3fa48d156e1ef10e8f86c574bc1d1b7ea
JuanMiguelElric <oliveirajuan243@gmail.com>
2024-02-07 14:31:51 -0300

    criação da migração do table funcionarios

---
+++ app/Http/Controllers/FuncionariosController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FuncionariosController extends Controller
+{
+    public function listarFuncionarios(){
+
+    }
+}
---
+++ resources/views/funcionarios/funcionarios.blade.php
@@ -0,0 +1,6 @@
+@extends('welcome')
+@section('conteudo')
+
+<div class="container">
+    <h1>Funcionários</h1>
+</div>
